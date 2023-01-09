f685e38e0bb9e8d46231621c7ae31a7cc8e04abd
Prashant Patil <remdev1@trojanonline.com>
2023-01-09 20:14:41 +0530

    Initial project setup

---
+++ Test/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            var str = "Revied15Error".ToCharArray().Where(x => !(x >= '0' && x <= '9')).ToArray();
+
+
+            Math m = new Math();
+
+            m.animalSound(); 
+
+            Console.ReadLine();
+
+ 
+        }
+    }
+
+
+    interface IMath
+    {
+        int Sum(int a, int b);
+        double Sum(double a, double b);
+    }
+
+
+    public class Math : Animal 
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("animalSound");
+        }
+         
+
+    }
+
+
+    public abstract class Animal
+    { 
+        public abstract void animalSound(); 
+
+        public Animal()
+        {
+            Console.WriteLine("constructor call");
+        }
+    }
+}
---
+++ Test/Program2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+      public class Program2
+    {
+
+        public virtual void method(string a)
+        {
+            Console.WriteLine("class program 2 method call:"+a);
+        }
+  
+    }
+
+    public class Program3 : Program2
+    {
+        public override void method(string a)
+        {
+            Console.WriteLine("class Program 3  method call:" + a);
+        }
+
+    }
+}
